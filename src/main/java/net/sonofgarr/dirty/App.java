99e04027753b3916b8c42839957195c0bf28b419
Mike McGarr <170149+jmcgarr@users.noreply.github.com>
2024-02-26 20:15:22 -0800

    Updating graal pluging (and some ancillary cleanup)

--- src/main/java/net/sonofgarr/dirty/App.java
+++ src/main/java/net/sonofgarr/dirty/App.java
@@ -11,7 +11,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 @Command(
